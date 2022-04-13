4a49f4115590d8b32012c1d96390f2fbbc0f7ea6
barabash_e <barabash.eugenia@palex.ru>
2022-04-13 10:40:45 +0700

    Made GroupCreationTest for task 3

--- addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
+++ addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
@@ -10,9 +10,39 @@ namespace addressbook_web_tests
         }
 
         [Test]
-        public void Test1()
+        public void TestSquare()
         {
-            Assert.Pass();
+            Square s1 = new Square(7);
+            Square s2 = new Square(10);
+            Square s3 = s1;
+
+            Assert.AreEqual(s1.Size, 7);
+            Assert.AreEqual(s2.Size, 10);
+            Assert.AreEqual(s3.Size, 7);
+
+            s3.Size = 15;
+            Assert.AreEqual(s1.Size, 15);
+
+            s2.Colored = true;
+            Assert.IsTrue(s2.Colored);
+        }
+
+        [Test]
+        public void TestCircle()
+        {
+            Circle s1 = new Circle(7);
+            Circle s2 = new Circle(10);
+            Circle s3 = s1;
+
+            Assert.AreEqual(s1.Radius, 7);
+            Assert.AreEqual(s2.Radius, 10);
+            Assert.AreEqual(s3.Radius, 7);
+
+            s3.Radius = 15;
+            Assert.AreEqual(s1.Radius, 15);
+
+            s2.Colored = false;
+            Assert.IsFalse(s2.Colored);
         }
     }
 }

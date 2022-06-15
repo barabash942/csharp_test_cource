ad9be2c7ec31128134bced50c8a48911e8d4371f
barabash_e <barabash.eugenia@palex.ru>
2022-06-15 17:20:08 +0700

    Lecture 9.1

---
+++ mantis-tests/appManager/RegistrationHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace mantis_tests
+{
+   public class RegistrationHelper : BaseHelper
+    {
+        public RegistrationHelper(ApplicationManager manager) : base(manager) { }
+
+        public void Register(AccountData account)
+        {
+            OpenMainPage();
+            OpenRegistrationForm();
+            FillRegistrationForm(account);
+            SubmitRegistration();
+        }
+
+        private void OpenRegistrationForm()
+        {
+            driver.FindElement(By.LinkText("Signup for a new account")).Click();
+        }
+
+        private void SubmitRegistration()
+        {
+            driver.FindElement(By.CssSelector("input.button")).Click();
+        }
+
+        private void FillRegistrationForm(AccountData account)
+        {
+            driver.FindElement(By.Name("username")).SendKeys(account.Name);
+            driver.FindElement(By.Name("email")).SendKeys(account.Email);
+        }
+
+        private void OpenMainPage()
+        {
+            manager.Driver.Url = "http://localhost/mantisbt-1.3.17/mantisbt-1.3.17/login_page.php";
+        }
+    }
+}
